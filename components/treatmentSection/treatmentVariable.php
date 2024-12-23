4f2e7c055a0f159b769b7799475179da4b59f852
papelashvili <khatopapelashvili@gmail.com>
2024-12-23 22:10:34 +0400

    Add modify feles

---
+++ components/treatmentSection/treatmentVariable.php
@@ -0,0 +1,38 @@
+
+<?php
+$treatments= [
+  [
+    "image"=>"./images/t1.png",
+    "title"=>"Nephrologist Care",
+    "description" =>"alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    "button"=>"#",
+    "buttonText"=>"Read More"
+
+  ],
+  [
+    "image"=>"./images/t2.png",
+    "title"=>"Eye Care",
+    "description" =>"alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    "button"=>"#",
+    "buttonText"=>"Read More"
+
+  ],
+  [
+    "image"=>"./images/t3.png",
+    "title"=>"Pediatrician Clinic",
+    "description" =>"alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    "button"=>"#",
+    "buttonText"=>"Read More"
+
+  ],
+  [
+    "image"=>"./images/t4.png",
+    "title"=>"Parental Care",
+    "description" =>"alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    "button"=>"#",
+    "buttonText"=>"Read More"
+
+  ]
+
+  
+];
